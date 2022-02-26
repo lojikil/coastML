63d3021148ea6418c79ecbf5015155230b491540
Stefan Edwards <saedwards.ecc@gmail.com>
2022-02-26 06:18:00 -0500

    interstitial commit of bootstrap

---
+++ boot.py
@@ -0,0 +1,70 @@
+import re
+import string
+
+class Token:
+    pass
+
+class TokenEOF(Token):
+    pass
+
+class TokenComment(Token):
+    def __init__(self, c, l, o):
+        self.lexeme = c
+        self.line = l
+        self.offset = o
+
+    def __repr__(self):
+        return "TokenComment({0})".format(self.lexeme)
+
+    def __str__(self):
+        return self.lexeme
+
+class Lex:
+    def __init__(self, src=None, offset=0, line=0):
+        self.src = src
+        self.offset = offset
+        self.line = line
+        self.rest_ident = re.compile("[a-zA-Z0-9-+=!@$%^&*|?]")
+        self.tag = re.compile("[A-Z][a-zA-Z0-9-+=!@$%^&*|?]*")
+        self.ident = re.compile("[a-z][a-zA-Z0-9-+=!@$%^&*|?]*")
+        self.keywords = re.compile("^(case|esac|fn|fc|cf|gn|type|epyt|mod)$")
+        self.types = re.compile("^(int|float|number|string|list|array|deque)$")
+
+    def next(self):
+        o = self.offset
+        if o >= len(self.src):
+            return TokenEOF()
+        elif self.src[o] in string.whitespace:
+            while o < len(self.src) and self.src[o] in string.whitespace:
+                if self.src[o] == '\n':
+                    self.line++
+                o++
+
+        # ok, now we've reached here, let's start lexing out some lexemes
+        if self.src[o] == '#':
+            no = o
+            while no < len(self.src) and self.src[no] != '\n':
+                no++
+            self.offset = no
+            self.line++
+            return TokenComment(self.src[o:no], self.line, self.offset)
+        # Let's start breaking down ideas here:
+        # we need:
+        #
+        # . namespaced items: `+Foo.Bar+`
+        # . accessors: `+_Foo+`
+        # . Keywords: (linguistic & type): `+case+`, `+esac+`
+        # . Tags: `+[A-Z][a-z0-9-+=!@$%^&*]+`
+        # . Identifiers: `+[a-z][a-z0-9-+=!@$%^&*]+`
+        # . Numbers: `+0b[0-1]+`, `+0o[0-7]+`, `+0x[0-9A-Fa-f]+`, `+[0-9]+`...
+        # . Dividers: {} [] () $()
+        elif self.src[o] == '$':
+            if self.src[o + 1] == '(':
+                self.offset += 2
+                return TokenCut(self.offset, self.line)
+            elif self.rest_ident.match(self.src[o + 1]):
+                no = o + 1
+                while no < len(self.src) and self.rest_ident.match(self.src[no]):
+                    no++
+                self.offset = no
+                return TokenIdent(self.src[o:no], self.line, self.offset)
