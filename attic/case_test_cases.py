7ba8fddf5cc5f1f9b0995970c131b043fa6d82d7
Stefan Edwards <saedwards.ecc@gmail.com>
2022-03-25 08:35:36 -0400

    notes on guard clauses...

--- attic/case_test_cases.py
+++ attic/case_test_cases.py
@@ -20,3 +20,17 @@ case
 | (x < y) { print "x < y"; }
 | _ { print "x == y"; }
 esac"""
+
+# This is interesting
+# how do we want to have guard clauses where
+# we are matching type constructors...
+#
+# hmm... well what about allowing nested constructions?
+# like `(Result.some (_1 > 10))`; that keeps things relatively
+# clean, but also allows us to guard these expressions nicely
+src3 = """
+case foo
+| (Result.Some 10) { print "foo is 10"; }
+| (Result.Some _) { print "foo is:" (_1 foo); }
+| (Result.None) { print "foo is none"; }
+esac"""
