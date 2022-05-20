d1488bd0b03476d0dd4d0b532b70daeb08332878
Stefan Edwards <saedwards.ecc@gmail.com>
2022-05-20 08:35:22 -0400

    added commandline interface to carpet python

--- carpet.py
+++ carpet.py
@@ -15,6 +15,7 @@
 #@(#) no real need to have an AST walker: just general Python3 from
 #@(#) the REPL and evaluate it in Python itself...
 
+import sys
 import re
 import string
 import functools
@@ -1917,3 +1918,32 @@ class CarpetPython:
         for ast in self.asts:
             self.generate_dispatch(ast, depth, tail=True)
             print("")
+
+if __name__ == "__main__":
+    print("main", sys.argv)
+    if len(sys.argv) != 3:
+        print("usage: carpet.py [command] [file]")
+        print("commands:\nload - load a file, and dump the resulting coastML")
+        print("python: dump python from a coastML file, without the compiler")
+        print("cpython: dump python from a coastML file, with the compiler")
+        print("note, the last two will be merged at some point")
+        sys.exit(0)
+
+    if sys.argv[1] == "load":
+        print("loading:", sys.argv[2])
+        with open(sys.argv[2]) as fh:
+            src = fh.read()
+            c = CoastalParser(src)
+            c.load()
+            print(c.sub_parse())
+    elif sys.argv[1] == "python":
+        print("pythonizing:", sys.argv[2])
+        with open(sys.argv[2]) as fh:
+            src = fh.read()
+            c = CarpetPython(src)
+            c.load()
+            c.generate()
+    elif sys.argv[1] == "cpython":
+        print("compiler + python: ", sys.argv[2])
+    else:
+        print("unknown command:", sys.argv[1])
