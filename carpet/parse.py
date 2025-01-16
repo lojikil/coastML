800290497c7f425b692b78660c3facb17dea3bac
Stefan Edwards <saedwards.ecc@gmail.com>
2025-01-16 10:46:52 -0500

    updating to support some new directions in keywords (basically reducing the number of keywords)

--- carpet/parse.py
+++ carpet/parse.py
@@ -457,7 +457,7 @@ class Lex:
         self.ns_adt = re.compile("[A-Z][a-zA-Z0-9_+=!@$%^&*|?<>/-]*(\.[A-Z][a-zA-Z0-9_+=!@$%^&*|?<>/-])+")
         self.ns_mod = re.compile("[A-Z][a-zA-Z0-9_+=!@$%^&*|?<>/-]*(::[a-zA-Z0-9_+=!@$%^&*|?<>/-])+")
         self.operators = re.compile("^([+=!@$%^&*|?<>/-])+$")
-        self.keywords = re.compile("^(case|esac|fn|fc|cf|gn|type|epyt|mod|is|box|sig|impl|newtype)$")
+        self.keywords = re.compile("^(case|esac|fn|type|epyt|mod|is|box|sig|impl|newtype)$")
         self.types = re.compile("^(int|float|number|string|list|array|deque|function|unit|bool|char|foreign|union|rho)$")
         self.bools = re.compile("^(true|false)$")
 
